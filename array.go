0c06b01d7d652ed07d56cb6af0b49350d62893f8
lvwenkai <lvwenkai@bytedance.com>
2021-06-24 12:07:14 +0800

    update
    Change-Id: Ie677a760a17229f2fc00e79f1cf6b25d3a5e710a

---
+++ array.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+func main() {
+	var a [2]string //这是数组
+	a[0] = "Hello"
+	a[1] = "World"
+	fmt.Println(a[0], a[1])
+	fmt.Println(a)
+
+	primes := [6]int{2, 3, 5, 7, 11, 13}
+	fmt.Println(primes)
+}
